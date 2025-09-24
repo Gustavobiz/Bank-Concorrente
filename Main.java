bbe71ec8ec01cf3299d713c198ff632b113df40a
Gustavobiz <gustavobernardes05@hotmail.com>
2025-09-24 16:33:06 -0300

    doc: coment

--- Main.java
+++ Main.java
@@ -9,8 +9,13 @@ public class Main {
         Thread t1 = new Thread(transferencia1, "Thread-1 (A->B)");
         Thread t2 = new Thread(transferencia2, "Thread-2 (B->A)");
 //debugg
-/*fff
+/*
  * 
+ * s
+ * s
+ * s
+ * s
+ * s
  */
 
         t1.start();
